ce622cfed353c5f09e7f7df8a5cc170fde384232
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-05-13 19:50:52 +0800

    removed payment spawn

--- Assets/Scripts/Customer/CustomerHand.cs
+++ Assets/Scripts/Customer/CustomerHand.cs
@@ -6,22 +6,15 @@ namespace Customer
     public class CustomerHand : MonoBehaviour
     {
         [SerializeField] private GameObject paymentPopUp;
-        [SerializeField] private Transform[] slots;
-        [SerializeField] private GameObject[] money;
+        
         public void OnMouseDown()
         {
-            print(true);
             if (paymentPopUp.activeSelf)
             {
                 paymentPopUp.SetActive(false);
                 return;
             }
             paymentPopUp.SetActive(true);
-            foreach (var slot in slots)
-            {
-                Instantiate(money[0], slot.position, Quaternion.identity);
-            }
-            
         }
     }
 }
