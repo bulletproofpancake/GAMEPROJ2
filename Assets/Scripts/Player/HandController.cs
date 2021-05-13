0067bb8cc20697e78d1cf773f11024daff4fe168
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-05-13 09:24:57 +0800

    Update HandController.cs
    Removed Lerped Movement

--- Assets/Scripts/Player/HandController.cs
+++ Assets/Scripts/Player/HandController.cs
@@ -7,25 +7,23 @@ namespace Player
    public class HandController : MonoBehaviour
     {
         private Camera _camera;
-        [SerializeField] private float moveSpeed;
-        [SerializeField] private bool isLerped;
 
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             _camera = Camera.main;
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
         {
             Vector2 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
-            if (!isLerped)
-            {
-                transform.position = mousePos;
-            }
-            else
-                transform.position = Vector3.Lerp(transform.position, mousePos, moveSpeed * Time.deltaTime);
+            transform.position = mousePos;
         }
     }
    
