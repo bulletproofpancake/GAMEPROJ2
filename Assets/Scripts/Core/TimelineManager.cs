b44f0e82ed618ab943ce57d42e37509cf1b3bbba
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-05-25 22:16:11 +0800

    timeline UI added

---
+++ Assets/Scripts/Core/TimelineManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core
+{
+    public class TimelineManager : MonoBehaviour
+    {
+        [SerializeField] private Slider timelineDisplay;
+
+        private void Update()
+        {
+            timelineDisplay.value += Time.deltaTime;
+            print(Time.deltaTime);
+        }
+    }
+}
