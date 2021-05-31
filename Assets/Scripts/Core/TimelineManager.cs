1bd75240f78eecb1e56707957ee6c4c7daec47e3
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-05-31 13:47:20 +0800

    Revert "added more timelines"
    This reverts commit 67ac8795685914e8361d39f8dd22687ef6bb7ecb.

--- Assets/Scripts/Core/TimelineManager.cs
+++ Assets/Scripts/Core/TimelineManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using Stations;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Core
@@ -6,38 +8,17 @@ namespace Core
     public class TimelineManager : MonoBehaviour
     {
         private GameManager _gameManager;
-        [SerializeField] private Slider[] timelineDisplay;
-
-        private int _currentStation = 0;
+        [SerializeField] private Slider timelineDisplay;
 
         private void Start()
         {
             _gameManager = FindObjectOfType<GameManager>();
-
-            foreach (var slider in timelineDisplay)
-            {
-                slider.maxValue = _gameManager.levelDuration;
-            }
-            
+            timelineDisplay.maxValue = _gameManager.levelDuration;
         }
 
         private void Update()
         {
-            DisplayTimeline();
+            timelineDisplay.value += Time.deltaTime;
         }
-
-        void DisplayTimeline()
-        {
-            if (_currentStation >= timelineDisplay.Length) return;
-            
-            timelineDisplay[_currentStation].value += Time.deltaTime;
-            
-            if (timelineDisplay[_currentStation].value >= timelineDisplay[_currentStation].maxValue)
-            {
-                //TODO: IMPLEMENT CUSTOMER ARRIVAL AND REMOVAL AT STATIONS
-                _currentStation++;
-            }
-        }
-        
     }
 }
