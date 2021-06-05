a547ad6b19f2d16f643004a3553e666c3b0cc7dd
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-06-05 22:07:20 +0800

    made customer spawner a prefab to transfer to main scene

---
+++ Assets/Scripts/Camera/Billboard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Following: https://youtu.be/_LRZcmX_xw0
+public class Billboard : MonoBehaviour
+{
+    [SerializeField] private Camera cameraToFollow;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
