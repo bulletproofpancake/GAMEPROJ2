c66150b520b306ce00ca21a83a454e74898f7db1
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-06-05 22:32:51 +0800

    billboard script finished, not used due to issues

--- Assets/Scripts/Camera/Billboard.cs
+++ Assets/Scripts/Camera/Billboard.cs
@@ -1,29 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 //Following: https://youtu.be/_LRZcmX_xw0
 public class Billboard : MonoBehaviour
 {
     [SerializeField] private Camera cameraToFollow;
-    
-    // Start is called before the first frame update
+
     void Start()
     {
-        SetCamera();
+        cameraToFollow = Camera.main;
+        //SetCamera();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.LookAt(cameraToFollow.transform);
+
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
     }
 
     void SetCamera()
     {
         //Goes through all cameras in the scene to look for the 2D camera
-        var cameras = FindObjectsOfType<Camera>();
-        
+        var cameras = FindObjectsOfType<UnityEngine.Camera>();
+
         foreach (var cam in cameras)
         {
             if (cam.CompareTag("2D Camera"))
@@ -31,7 +30,6 @@ public class Billboard : MonoBehaviour
                 cameraToFollow = cam;
             }
         }
-        
     }
-    
 }
+
