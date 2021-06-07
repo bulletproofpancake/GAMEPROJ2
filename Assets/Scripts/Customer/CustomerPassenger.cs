f8c9444c39ee661807a47ec3c2491f0b63e2c5c7
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-06-07 20:45:59 +0800

    customer hand spawns when jeep collides with passenger

---
+++ Assets/Scripts/Customer/CustomerPassenger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Customer
+{
+    public class CustomerPassenger : MonoBehaviour
+    {
+        [HideInInspector] public CustomerManager customerManager;
+
+        private void Start()
+        {
+            customerManager = FindObjectOfType<CustomerManager>();
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!other.gameObject.CompareTag("Player")) return;
+            
+            customerManager.Spawn();
+            Destroy(gameObject);
+        }
+    }
+}
