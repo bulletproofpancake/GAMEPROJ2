f2fb08ae5c5eb9a04b33c5d12fa1d5e87146b25c
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-06-08 15:04:14 +0800

    passenger spawner now spawns

--- Assets/Scripts/Customer/CustomerManagerJeepney.cs
+++ Assets/Scripts/Customer/CustomerManagerJeepney.cs
@@ -1,9 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Customer
 {
     public class CustomerManagerJeepney : MonoBehaviour
     {
+        [SerializeField] private GameObject[] passengers;
+        [SerializeField] private float timeToSpawnCap;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Spawn();
+            }
+        }
+
+        private void Spawn()
+        {
+            var passenger = Instantiate(passengers[Random.Range(0, passengers.Length)],transform.parent);
+        }
         
     }
 }
