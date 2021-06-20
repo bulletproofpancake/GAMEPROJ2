3e62b27a433a79668c3b38b61cd4bc0d66a1c313
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-06-20 15:43:14 +0800

    passengers now "jumps" to jeep

--- Assets/Scripts/Customer/CustomerPassenger.cs
+++ Assets/Scripts/Customer/CustomerPassenger.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using Cinemachine;
 using UnityEngine;
 
 namespace Customer
@@ -6,30 +8,45 @@ namespace Customer
     public class CustomerPassenger : MonoBehaviour
     {
         [HideInInspector] public CustomerManagerPayment customerManager;
+        private float _jumpSpeed = 0.05f;
+        private bool _isBoarding;
+        private GameObject jeepObject;
 
         private void Start()
         {
             customerManager = FindObjectOfType<CustomerManagerPayment>();
         }
 
-        // private void OnCollisionEnter(Collision other)
-        // {
-        //     if (!other.gameObject.CompareTag("Player")) return; 
-        //     
-        //     if (customerManager.areSeatsFull) return;
-        //     
-        //     customerManager.Spawn();
-        //     Destroy(gameObject);
-        // }
-
-        private void OnTriggerEnter(Collider other)
+        private void OnCollisionEnter(Collision other)
         {
             if (!other.gameObject.CompareTag("Player")) return; 
             
             if (customerManager.areSeatsFull) return;
             
             customerManager.Spawn();
+            
             Destroy(gameObject);
+            
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.gameObject.CompareTag("Player")) return;
+
+            if (customerManager.areSeatsFull) return;
+
+            _isBoarding = true;
+
+            jeepObject = other.gameObject;
+
+        }
+
+        private void Update()
+        {
+            if (_isBoarding)
+            {
+                transform.position = Vector3.Lerp(transform.position, jeepObject.transform.position, _jumpSpeed);
+            }
         }
     }
 }
