6a20d7463946816398a65a0ae436e4f2412f51b7
Georson Chua <71176893+zerogeorson@users.noreply.github.com>
2021-07-06 03:03:25 +0800

    TitleScreen (99%) kulang smoke

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,23 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+
+     [SerializeField]
+    private GameObject thing;
+
+    [SerializeField]
+    private Animator anim;
+
+    [SerializeField]
+    private float animationDuration;
+
+    void Start() 
+    {
+      
+       anim = thing.GetComponent<Animator>();
+    }
+
     public void Load()
     {
+         anim.SetBool("isStart", false);
         StartCoroutine(LoadRoutine());
     }
 
     private IEnumerator LoadRoutine()
     {
         //Lahat ng animations mo ilagay mo dito
-        /*
-            //animation code 
-        */ 
-        
+        anim.SetBool("isStart", true);
+
         //Ilagay mo dun sa animation duration yung haba nung animation na gagawin mo para saka lang magloload kapag tapos na yung animation
-        //yield return new WaitForSeconds(animationDuration);
+        yield return new WaitForSeconds(animationDuration);
         
         //Comment out mo to pag meron ka nang duration
-        yield return null;
+        //yield return null;
         
         SceneLoader.Instance.LoadGame();
     }
