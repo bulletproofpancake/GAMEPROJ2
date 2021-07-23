eea4da6e5bcf43c72ba4510bc5d4ce9c6e8bebbd
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-07-23 16:10:54 +0800

    fixed speed indicator in build

--- Assets/Scripts/SpeedIndicator.cs
+++ Assets/Scripts/SpeedIndicator.cs
@@ -8,12 +8,12 @@ public class SpeedIndicator : Singleton<SpeedIndicator>
     public Image indicator;
     public Image[] speed;
     private int index;
-
+    //private float offset = 18.75f;
     public void SpeedUp()
     {
         index++;
         if (index <= 2)
-            indicator.transform.position = speed[index].transform.position + new Vector3(12.5f,12.5f);
+            indicator.transform.position = speed[index].transform.position + new Vector3(25f,25f);
         else
         {
             index = 2;
@@ -23,6 +23,6 @@ public class SpeedIndicator : Singleton<SpeedIndicator>
     public void Stop()
     {
         index = 0;
-        indicator.transform.position = speed[index].transform.position + new Vector3(12.5f,12.5f);
+        indicator.transform.position = speed[index].transform.position + new Vector3(25f,25f);
     }
 }
